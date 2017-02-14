4adc00c9dabc8b3ff96fb9efd58b2a1538e56b4b
Jingyi Cao <jcao93@uwo.ca>
2017-02-14 15:52:34 -0500

    Add course class and made changes to Student and Instructor.

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by RaspberryMonster on 2017/2/14.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname,String description){
+        this.cname=cname;
+        this.description=description;
+    }
+}
